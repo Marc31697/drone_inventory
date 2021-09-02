83ece966df424f2d06c25539d72cb3a8463044ba
Marc31697 <mbrown.bmhs1@hotmail.com>
2021-09-02 14:36:16 -0400

    Second commit

--- config.py
+++ config.py
@@ -14,5 +14,5 @@ class Config():
     """
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or "You will never guess"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off update messages from the sqlalchemy
